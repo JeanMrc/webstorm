bd78777eb505511316180e1cd701557e1ebd443d
JeanMrc <157700454+JeanMrc@users.noreply.github.com>
2025-04-21 10:47:51 +0700

    Add files via upload

---
+++ Webstorm/script.js
@@ -0,0 +1,11 @@
+function handlePreOrder() {
+  alert("Thanks for your interest! Pre-orders coming soon 🚀");
+}
+
+document.querySelector('.footer-btn:nth-child(1)').onclick = () => {
+  alert("Terms & Conditions content goes here...");
+};
+
+document.querySelector('.footer-btn:nth-child(3)').onclick = () => {
+  alert("Privacy Policy content goes here...");
+};
